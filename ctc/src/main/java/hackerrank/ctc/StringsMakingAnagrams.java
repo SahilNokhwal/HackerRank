2064911bca12ee8270e2be700eddb2ec839428c8
Sahil Nokhwal <nokhwal4357@gmail.com>
2016-12-01 19:39:29 -0600

    Add new files

---
+++ ctc/src/main/java/hackerrank/ctc/StringsMakingAnagrams.java
@@ -0,0 +1,18 @@
+package hackerrank.ctc;
+
+import java.util.Scanner;
+public class StringsMakingAnagrams {
+	
+	
+	
+    public static int numberNeeded(String first, String second) {
+      
+    }
+  
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        System.out.println(numberNeeded(a, b));
+    }
+}
